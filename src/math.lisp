ca03cd0cae03466cb5a419edf93d516a17bea491
marsha88 <marsha88@purdue.edu>
2017-07-30 19:54:41 -0400

    generalized minmax/minmaxBy functions.

--- src/math.lisp
+++ src/math.lisp
@@ -6,34 +6,35 @@
 ;; divideBy: accepts a single divisor and returns a function that accepts a dividend and returns the quotient of the dividend and divisor
 (defun _divideBy (divisor) (lambda (dividend) (/ dividend divisor)))
 
-;;;TODO: GENERALIZE THE MAX/MIN PROCEDURES
+;; minmax: helper function to generalize min/max functions
+(defun minmax (nlist op)
+  (defun minmax-iter (arr est)
+    (if (null arr) est
+      (if (funcall op (car arr) est)
+        (minmax-iter (cdr arr) (car arr))
+        (minmax-iter (cdr arr) est))))
+
+  (minmax-iter (cdr nlist) (car nlist))
+)
+
+;; minmaxBy: helper function to generalize minBy/maxBy functions
+(defun minmaxBy (plist proc op)
+  (defun minmaxBy-iter (arr est)
+    (if (null arr) est
+      (if (funcall op (funcall proc (car arr)) (funcall proc est))
+        (minmaxBy-iter (cdr arr) (car arr))
+        (minmaxBy-iter (cdr arr) est))))
+
+  (minmaxBy-iter (cdr plist) (car plist))
+)
 
 ;; max: given a list returns the max value in the list
 (defun _max (nlist)
-  (defun max-iter (arr highest)
-    (if (null arr) highest
-      (if (> (car arr) highest)
-        (max-iter (cdr arr) (car arr))
-        (max-iter (cdr arr) highest))))
-
-  (max-iter (cdr nlist) (car nlist))
-)
+  (_minmax nlist #'>))
 
 ;; maxBy: accepts a list and a function to determine the rank of the elements within the list. Returns the highest ranked element within the list.
 (defun _maxBy (plist proc)
-  (defun maxBy-iter (arr highest)
-    (if (null arr) highest
-      (if (> (funcall proc (car arr)) (funcall proc highest))
-        (maxBy-iter (cdr arr) (car arr))
-        (maxBy-iter (cdr arr) highest))))
-
-  (maxBy-iter (cdr plist) (car plist))
-)
-
-;; helper function for finding length of a list
-(defun length (arr)
-  (if (null arr) 0
-    (+ 1 (length (cdr arr)))))
+  (minmaxBy plist proc #'>))
 
 ;; mean: given a list of numbers returns the mean of the values in the list
 (defun _mean (arr)
@@ -43,32 +44,18 @@
 (defun _meanBy (arr proc)
   (/ (_sumBy arr proc) (length arr)))
 
-;; min: given a list and a criterion function to rank items returns the smallest ranked item in the list
-(defun _min (nlist proc)
-  (defun maxBy-iter (arr highest)
-    (if (null arr) highest
-      (if (< car arr) highest)
-        (maxBy-iter (cdr arr) (car arr))
-        (maxBy-iter (cdr arr) highest)))
+;; min: given a list, returns the smallest ranked item in the list
+(defun _min (nlist)
+  (_minmax nlist #'<))
 
-  (maxBy-iter (cdr nlist) (car nlist))
-)
-
-;; minBy: accepts a list and a function to determine the rank of the elements within the list. Returns the lowest ranked element within the list.
+;; minBy: accepts a list and a function to determine the rank of the elements within the list. Returns element with lowest rank.
 (defun _minBy (plist proc)
-  (defun minBy-iter (arr lowest)
-    (if (null arr) lowest
-      (if (< (funcall proc (car arr)) (funcall proc lowest))
-        (maxBy-iter (cdr arr) (car arr))
-        (maxBy-iter (cdr arr) lowest))))
+  (minmaxBy plist proc #'<))
 
-  (minBy-iter (cdr plist) (car plist))
-)
-
-;; multiply accepts two numbers and returns their product
+;; multiply accepts a multiplicand and returns a function to accept a multiplier and return their product
 (defun _multiplyBy (multiplicand) (lambda (multiplier) (* multiplier multiplicand)))
 
-;; subtract: accepts two numbers and returns the first - second
+;; subtract: accepts a single subtrahend and returns a function that accepts another number and returns the difference
 (defun _subtractBy (subtrahend) (lambda (minuend) (- minuend subtrahend)))
 
 ;; sum: takes a list of numbers and returns their sum

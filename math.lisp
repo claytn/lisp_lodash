b14ab848e171dbda5bd4400f30597284d2cac3ee
marsha88 <marsha88@purdue.edu>
2017-07-30 15:26:01 -0400

    added minBY, maxBY, sumBY, and meanBY

--- math.lisp
+++ math.lisp
@@ -1,55 +1,83 @@
 ;;; Lisp Implemenations of Lodash Math functions
 
-;;TODO: reimplement simple math operations similar to rambda's operationBy functions.
+;; addBy: accepts a single addend and returns a function that will add a given number by the addend
+(defun _addBy (addend) (lambda (augend) (+ augend addend)))
 
-;; add: accepts two numbers and returns their sum
-(defun _add (augend addend) (+ augend addend))
+;; divideBy: accepts a single divisor and returns a function that accepts a dividend and returns the quotient of the dividend and divisor
+(defun _divideBy (divisor) (lambda (dividend) (/ dividend divisor)))
 
-;; divide: accepts two numbers and returns their quotient
-(defun _divide (dividend divisor) (/ dividend divisor))
-
+;;;TODO: GENERALIZE THE MAX/MIN PROCEDURES
 
 ;; max: given a list returns the max value in the list
-(defun _max (arr)
-  (defun maxBy-iter (arr highest)
+(defun _max (nlist)
+  (defun max-iter (arr highest)
     (if (null arr) highest
       (if (> (car arr) highest)
+        (max-iter (cdr arr) (car arr))
+        (max-iter (cdr arr) highest))))
+
+  (max-iter (cdr nlist) (car nlist))
+)
+
+;; maxBy: accepts a list and a function to determine the rank of the elements within the list. Returns the highest ranked element within the list.
+(defun _maxBy (plist proc)
+  (defun maxBy-iter (arr highest)
+    (if (null arr) highest
+      (if (> (funcall proc (car arr)) (funcall proc highest))
         (maxBy-iter (cdr arr) (car arr))
         (maxBy-iter (cdr arr) highest))))
 
-  (maxBy-iter (cdr arr) (car arr))
+  (maxBy-iter (cdr plist) (car plist))
 )
 
-;; length: helper function that computes the length of a list
+;; helper function for finding length of a list
+(defun length (arr)
+  (if (null arr) 0
+    (+ 1 (length (cdr arr))))
+)
 
 ;; mean: given a list of numbers returns the mean of the values in the list
 (defun _mean (arr)
-  (defun length (arr)
-    (if (null arr) 0
-      (+ 1 (length (cdr arr))))
-  )
-  (/ (_sum arr) (length arr))
-)
+  (/ (_sum arr) (length arr)))
+
+;; meanBy: given a list and a function, returns the mean of the values returned by the passed in function.
+(defun _meanBy (arr proc)
+  (/ (_sumBy arr proc) (length arr)))
 
 ;; min: given a list and a criterion function to rank items returns the smallest ranked item in the list
-(defun _min (arr proc)
+(defun _min (nlist proc)
   (defun maxBy-iter (arr highest)
     (if (null arr) highest
-      (if (< (proc (car arr)) (proc highest))
+      (if (< car arr) highest)
         (maxBy-iter (cdr arr) (car arr))
-        (maxBy-iter (cdr arr) highest))))
+        (maxBy-iter (cdr arr) highest)))
 
-  (maxBy-iter (cdr arr) (car arr))
+  (maxBy-iter (cdr nlist) (car nlist))
+)
+
+;; minBy: accepts a list and a function to determine the rank of the elements within the list. Returns the lowest ranked element within the list.
+(defun _minBy (plist proc)
+  (defun minBy-iter (arr lowest)
+    (if (null arr) lowest
+      (if (< (funcall proc (car arr)) (funcall proc lowest))
+        (maxBy-iter (cdr arr) (car arr))
+        (maxBy-iter (cdr arr) lowest))))
+
+  (minBy-iter (cdr plist) (car plist))
 )
 
 ;; multiply accepts two numbers and returns their product
-(defun _multiply (multiplier multiplicand) (* multiplier multiplicand))
+(defun _multiplyBy (multiplicand) (lambda (multiplier) (* multiplier multiplicand)))
 
 ;; subtract: accepts two numbers and returns the first - second
-(defun _subtract (minuend subtrahend) (- minuend subtrahend))
+(defun _subtractBy (subtrahend) (lambda (minuend) (- minuend subtrahend)))
 
 ;; sum: takes a list of numbers and returns their sum
 (defun _sum (nums)
   (if (null nums) 0
-    (+ (car nums) (_sum (cdr nums))))
-)
+    (+ (car nums) (_sum (cdr nums)))))
+
+;; sumBy: accepts a list and a function which determines the value to be summed for a single list entry.
+(defun _sumBy (arr proc)
+  (if (null arr) 0
+    (+ (funcall proc (car arr)) (_sum (cdr arr)))))
